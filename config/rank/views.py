b2ef2fa717dd587d1309e0ebf76521dff0c61a4b
kim-su-mi <ksmm9607@naver.com>
2024-09-18 00:05:55 +0900

    completed_photo_ing

--- config/rank/views.py
+++ config/rank/views.py
@@ -11,40 +11,40 @@ from rest_framework.decorators import permission_classes
 
 # Create your views here.
 ##우리팀 진행사항 반영
-# class BingoProgressView(APIView):
-#     @permission_classes([IsAuthenticated])
-#     def get(self, request, *args, **kwargs):
-#         # 쿼리 파라미터에서 bingo_id와 team_id를 가져옴
-#         bingo_id = request.query_params.get('bingo_id')
-#         team_id = request.query_params.get('team_id')
+class BingoProgressView(APIView):
+    @permission_classes([IsAuthenticated])
+    def get(self, request, *args, **kwargs):
+        # 쿼리 파라미터에서 bingo_id와 team_id를 가져옴
+        bingo_id = request.query_params.get('bingo_id')
+        team_id = request.query_params.get('team_id')
 
-#         # 필수 파라미터가 누락된 경우 에러 응답 반환
-#         if not bingo_id or not team_id:
-#             return Response({'error': 'bingo_id나 team_id를 넘겨주세요'}, status=status.HTTP_400_BAD_REQUEST)
+        # 필수 파라미터가 누락된 경우 에러 응답 반환
+        if not bingo_id or not team_id:
+            return Response({'error': 'bingo_id나 team_id를 넘겨주세요'}, status=status.HTTP_400_BAD_REQUEST)
         
-#         # bingo_id에 해당하는 Bingo 객체를 가져옵니다.
-#         bingo = get_object_or_404(Bingo, id=bingo_id)
+        # bingo_id에 해당하는 Bingo 객체를 가져옵니다.
+        bingo = get_object_or_404(Bingo, id=bingo_id)
         
-#         # team_id에 해당하는 Team 객체를 가져옵니다.
-#         team = get_object_or_404(BingoProgress, team_id=team_id, bingo=bingo)
+        # team_id에 해당하는 Team 객체를 가져옵니다.
+        team = get_object_or_404(BingoProgress, team_id=team_id, bingo=bingo)
         
-#         # 해당 팀의 사용자(User_Team) 정보를 가져옵니다.
-#         user_team_members = User_Team.objects.filter(team=team.team, bingo=bingo)
+        # 해당 팀의 사용자(User_Team) 정보를 가져옵니다.
+        user_team_members = User_Team.objects.filter(team=team.team, bingo=bingo)
 
-#         # 해당 팀의 진행 상황과 유저 이름을 포함한 정보를 반환
-#         result = {
-#             'team_name': team.team.team_name,
-#             'completed_bingo_count': team.completed_bingo_count,
-#             'completed_cell_count': team.completed_cell_count
-#         }
+        # 해당 팀의 진행 상황과 유저 이름을 포함한 정보를 반환
+        result = {
+            'team_name': team.team.team_name,
+            'completed_bingo_count': team.completed_bingo_count,
+            'completed_cell_count': team.completed_cell_count
+        }
 
-#         return Response(result, status=status.HTTP_200_OK)
+        return Response(result, status=status.HTTP_200_OK)
     
 ##팀별 진행사항 반영
     #특정 bingo_id에 대한 팀별 진행 현황을 가져오고, 
     #completed_bingo_count 기준으로 정렬, 동률 시 completed_cell_count로 정렬.
     #그리고 각 팀에 속한 사용자의 이름도 반환.
-class BingoProgressView(APIView):
+class BingoProgressTotalView(APIView):
     @permission_classes([IsAuthenticated])
     def get(self, request, *args, **kwargs):
         # 쿼리 파라미터에서 bingo_id를 가져옴
